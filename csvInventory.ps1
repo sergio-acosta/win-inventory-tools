2bd8fd6ed76fab48defb6b237bfb0e738fa8a7ba
Sergio Acosta <sergio.acostahernandez@gmail.com>
2017-05-24 11:08:57 +0200

    Developing the script in PowerShell

--- csvInventory.ps1
+++ csvInventory.ps1
@@ -3,7 +3,7 @@
 .SYNOPSIS
   This script generates a CSV file with asset information. 
 .DESCRIPTION
-  The script will create a .CSV file about the local computer or a remote computer if its hostname is provided as argument (see below)
+  The script will create a CSV file about the local computer or a remote computer if its hostname is provided as argument (see below)
 .PARAMETER <hostname>
   If provided, generates the report about a remote computer.
 .INPUTS
@@ -15,7 +15,8 @@
   Author:         Sergio Acosta
   Creation Date:  17/01/2016
   Purpose/Change: -
-  
 .EXAMPLE
   csvInventory.ps1 localhost
 #>
+
+
