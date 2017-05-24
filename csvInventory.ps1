d5a1ddffb589bd6550f61a6e9976ef012b816deb
Sergio Acosta <sergio.acostahernandez@gmail.com>
2017-05-24 11:11:02 +0200

    Better format

--- csvInventory.ps1
+++ csvInventory.ps1
@@ -1,22 +1,23 @@
 #requires -version 2
 <#
 .SYNOPSIS
-  This script generates a CSV file with asset information. 
+	This script generates a CSV file with asset information. 
 .DESCRIPTION
-  The script will create a CSV file about the local computer or a remote computer if its hostname is provided as argument see below
-.PARAMETER <hostname>
-  If provided, generates the report about a remote computer.
+	The script will create a CSV file about the local computer or a remote computer if its hostname is provided as argument see below
+.PARAMETER 
+	<hostname>
+If provided, generates the report about a remote computer.
 .INPUTS
-  None
+	None
 .OUTPUTS
-  Comma-separated file <hostname>.csv 
+	Comma-separated file <hostname>.csv 
 .NOTES
-  Version:        1.0
-  Author:         Sergio Acosta
-  Creation Date:  17/01/2016
-  Purpose/Change: -
+	Version:        1.0
+	Author:         Sergio Acosta
+	Creation Date:  17/01/2016
+	Purpose/Change: -
 .EXAMPLE
-  csvInventory.ps1 localhost
+	csvInventory.ps1 localhost
 #>
 
 
